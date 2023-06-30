c982351eaed0b213a00e73f9cb292c9e42754b30
mmintsdev <mmintsdev@gmail.com>
2023-06-30 16:34:48 +0300

    1: Homework1 -> "Current time"

---
+++ src/homework1/currentTime.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+// Program that prints the current time / exact time using the NTP library
+
+func main() {
+	println("Start")
+	CurrentTime()
+	println("End")
+	os.Exit(0)
+}
+
+func CurrentTime() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("Current time: %v\n", time)
+}
